6bfb25ae0be76522cc5c5e6a9b4f7067d5cc0a16
holden <al1onggwp@gmail.com>
2026-03-26 19:55:36 +0500

    fix 2

--- app/server.ts
+++ app/server.ts
@@ -1,9 +1,12 @@
 import http from 'http';
 
-export const server = http.createServer();
+export const server = http.createServer((_, res) => {
+  res.writeHead(200);
+  res.end('OK');
+});
 
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on ${PORT}`);
 });
