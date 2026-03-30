e03d5056fe149818f49ac5ad0f8fc7a7c5aa7e70
holden <al1onggwp@gmail.com>
2026-03-30 23:25:34 +0500

    fix minor issue

--- app/socket.ts
+++ app/socket.ts
@@ -6,25 +6,34 @@ const ws = new WebSocketServer({ server });
 interface IMessage {
   type: string;
   roomname?: string;
-  username?: string;
+  userName?: string;
   roomId?: string;
+  streamId?: string;
 }
 
-function sendMessage(roomId: string | undefined, socket: WebSocket, msg: unknown) {
+function sendMessage(
+  roomId: string | undefined,
+  socket: WebSocket,
+  msg: unknown,
+) {
   if (!roomId) return;
   const room = rooms.get(roomId);
   if (!room) return;
-  const recievers = room.clients.filter((s) => s !== socket);
+  const recievers = room.clients.filter((s) => s.socket !== socket);
   recievers.forEach((r) => {
-    if (r.readyState === WebSocket.OPEN) {
-      r.send(JSON.stringify(msg));
+    if (r.socket.readyState === WebSocket.OPEN) {
+      r.socket.send(JSON.stringify(msg));
     }
   });
 }
 
 type Room = {
   roomName: string;
-  clients: WebSocket[];
+  clients: {
+    streamId: string;
+    userName: string;
+    socket: WebSocket;
+  }[];
 };
 const rooms = new Map<string, Room>();
 
@@ -37,7 +46,13 @@ ws.on("connection", (socket: WebSocket) => {
         case "createRoom":
           rooms.set(roomId!, {
             roomName: parsed.roomname!,
-            clients: [socket],
+            clients: [
+              {
+                streamId: "",
+                userName: "",
+                socket,
+              },
+            ],
           });
           break;
         case "joinRoom":
@@ -45,14 +60,41 @@ ws.on("connection", (socket: WebSocket) => {
           if (!currentRoom || !roomId || currentRoom.clients.length > 1) return;
           rooms.set(roomId, {
             roomName: currentRoom.roomName,
-            clients: [...currentRoom.clients, socket],
+            clients: [
+              ...currentRoom.clients,
+              {
+                userName: "",
+                streamId: "",
+                socket,
+              },
+            ],
           });
           if (socket.readyState === WebSocket.OPEN) {
-            socket.send(JSON.stringify({ type: "self-joined", roomId, roomName: currentRoom.roomName}));
+            socket.send(
+              JSON.stringify({
+                type: "self-joined",
+                roomId,
+                roomName: currentRoom.roomName,
+                clients: currentRoom.clients.filter((c) => c.socket !== socket),
+              }),
+            );
           }
-          sendMessage(roomId, socket, { type: "joined", userName: parsed.username});
+          sendMessage(roomId, socket, {
+            type: "joined",
+            userName: parsed.userName,
+          });
           break;
         default:
+          if (type === "joined-metadata") {
+            const room = rooms.get(roomId!);
+            const currentClient = room?.clients.find(
+              (c) => c.socket === socket,
+            );
+            if (currentClient) {
+              currentClient.streamId = parsed.streamId!;
+              currentClient.userName = parsed.userName!;
+            }
+          }
           sendMessage(roomId, socket, parsed);
       }
     } catch (e) {
